ca80f53b503c8bdb9ba5564ec9ad004c3b59f2e0
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-14 06:02:50 +0100

    scaffolding and created headerapp

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HeaderApp />
+</template>
+
+<script>
+import HeaderApp from './components/HeaderApp.vue';
+  export default {
+    components:{
+      HeaderApp
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="d-flex">
+        <div>
+            <img src="/images/yu-gi-oh-logo.jpg" alt="yu-gi-oh-logo">
+        </div>
+        <h1>
+            Yu-Gi-Oh-Api
+        </h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "HeaderApp"
+    }
+</script>
+
+<style lang="scss" scoped>
+    header{
+        padding:20px;
+        img{
+            width:150px
+        }
+    }
+</style>
