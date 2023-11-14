f1647460bb64cdf1114af3fbb3195d90259c7c48
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-11-14 06:12:56 +0100

    imported cards in app.vue with store.js

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+import {store} from './data/store';
 import HeaderApp from './components/HeaderApp.vue';
   export default {
     components:{
       HeaderApp
+    },
+    data(){
+      return{
+        store
+      }
+    },
+    methods:{
+      getCards(){
+        const pag1Url = store.apiUrl + store.page1
+        axios.get(pag1Url).then((response) =>{
+          store.cardArray = response.data.data;
+          console.log(store.cardArray)
+        })
+      }
+      
+    },
+    created(){
+      this.getCards();
     }
   }
 </script>
